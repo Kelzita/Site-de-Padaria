c6f45cdf749bc915bc71f02f430d45866bf27f78
Kelzita <raquelfernandesbriyoidjdj@gmail.com>
2025-08-22 17:00:38 -0300

    banco de dados

--- Banco de dados/padaria_pao_genial.sql
+++ Banco de dados/padaria_pao_genial.sql
@@ -197,7 +197,8 @@ CREATE TABLE `produto` (
   `quantidade` int(11) NOT NULL,
   `preco` decimal(10,2) DEFAULT NULL,
   `unmedida` char(10) DEFAULT NULL,
-  `validade` date DEFAULT NULL
+  `validade` date DEFAULT NULL,
+  `imagem_produto` longblob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
