86872739d0573b13f772ac55e3ef87e39a5a0997
raquelfernandes <raquelfernandesbriyoidjdj@gmail.com>
2025-08-23 23:05:19 -0300

    Página inicial

---
+++ home.php
@@ -0,0 +1,20 @@
+  <?php 
+  session_start();
+  require_once 'php/conexao.php';
+  require_once 'php/menu.php';
+  
+  ?>
+  <!DOCTYPE html>
+  <html lang="PT-BR">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Início</title>
+  </head>
+  <body>
+    <header>
+        <div class="menu-icon"></div>
+    </header
+  
+  </body>
+  </html>
