746fe397cbd6ac395e03d65b3a3d2e0f125ed2f2
raquelfernandes <raquelfernandesbriyoidjdj@gmail.com>
2025-08-23 23:05:49 -0300

    Tela de Login

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap">
+    <link rel="stylesheet" href="css/stylelogin.css">
+    <title>Login</title>
+</head>
+<body>
+    <body>
+        <div class="login">
+          <form action="php/login.php" method="post" class="formulario-style">
+            <h1 class="titulo-style">Login</h1>
+      
+            <div class="conteudo">
+              <div class="caixa-style">
+                <i class="ri-user-3-line login__icon"></i>
+                <div class="campo-input">
+                  <input name="nome_usuario" type="text" class="style-input" id="nome_usuario" placeholder=" ">
+                  <label for="nome_usuario" class="style-label">Usuário</label>
+                </div>
+              </div>
+      
+              <div class="caixa-style">
+                <i class="ri-lock-2-line login__icon"></i>
+                <div class="campo-input">
+                  <i class="ri-eye-off-line login__eye" id="login-eye" onclick="toggleSenha()"></i>
+                  <input name="senha" type="password" class="style-input" id="senha" placeholder=" ">
+                  <label for="senha" class="style-label">Senha</label>
+                </div>
+              </div>
+            </div>
+      
+            <div class="link-auxiliar">
+              <a href="redefinirsenha.html" class="esqueci-senha-style">Esqueceu a senha?</a>
+            </div>
+      
+            <button type="submit" class="botao-style" onclick="VerificarCampos()">Login</button>
+          </form>
+        </div>
+</body>
+<script>
+    function toggleSenha() {
+        const inputSenha = document.getElementById("senha");
+        const botao = document.getElementById("login-eye");
+
+        if (inputSenha.type === "password") {
+            inputSenha.type = "text";
+            botao.classList.remove("ri-eye-off-line");
+            botao.classList.add("ri-eye-line");
+        } else {
+            inputSenha.type = "password";
+            botao.classList.remove("ri-eye-line");
+            botao.classList.add("ri-eye-off-line");
+        }
+    }
+</script>
+
+</html>
