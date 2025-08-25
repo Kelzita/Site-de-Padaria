6b0d527a8cb05cd6b9fb6d5393bcda34f7139189
raquelfernandes <raquelfernandesbriyoidjdj@gmail.com>
2025-08-25 15:55:26 -0300

    commit

--- index.php
+++ index.php
@@ -41,21 +41,6 @@
           </form>
         </div>
 </body>
-<script>
-    function toggleSenha() {
-        const inputSenha = document.getElementById("senha");
-        const botao = document.getElementById("login-eye");
-
-        if (inputSenha.type === "password") {
-            inputSenha.type = "text";
-            botao.classList.remove("ri-eye-off-line");
-            botao.classList.add("ri-eye-line");
-        } else {
-            inputSenha.type = "password";
-            botao.classList.remove("ri-eye-line");
-            botao.classList.add("ri-eye-off-line");
-        }
-    }
-</script>
+<script src="js/togglesenha.js"></script>
 
 </html>
