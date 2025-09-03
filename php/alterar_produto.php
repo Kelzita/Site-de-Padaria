de5057aeb4edfeb1f1c667b151f63bfb68a24a0d
Emanuela1304 <emanuela_dambros@estudante.sesisenai.org.br>
2025-09-03 15:35:46 -0300

    .......

--- php/alterar_produto.php
+++ php/alterar_produto.php
@@ -43,7 +43,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alterar Produto</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style">
     <!-- certifique-se que o javascript esta sendo carregado corretamente -->
     <script src="scripts.js"></script>
 </head>
