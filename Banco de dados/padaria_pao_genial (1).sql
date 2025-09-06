afb5e64c274f7fdf53e75fc4e270b3da04f8a82f
Jamilly <jamillyrodriguesfroes@gmail.com>
2025-09-06 13:14:18 -0300

    bd

--- Banco de dados/padaria_pao_genial (1).sql
+++ Banco de dados/padaria_pao_genial (1).sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `padaria_pao_genial`
 --
-
+create database padaria_pao_genial;
+use padaria_pao_genial;
 -- --------------------------------------------------------
 
 --
