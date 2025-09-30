38f0eec46762b613e2ff0098e11cfc6bb67295a6
raquelfernandes <raquelfernandesbriyoidjdj@gmail.com>
2025-09-30 15:56:02 -0300

    Revert "aaaaa"
    This reverts commit 16d89cf7034a7c699b12c910fef044d2c3e23ab3.

--- dockerfile
+++ dockerfile
@@ -4,14 +4,4 @@ FROM php:8.2-apache
 # Instalar extensões necessárias
 RUN docker-php-ext-install pdo pdo_mysql mysqli
 
-# Ativar mod_rewrite do Apache
-RUN a2enmod rewrite
-
-# Configurar diretório de trabalho
-WORKDIR /var/www/html
-
-# Copiar arquivos do projeto para o container
-COPY . /var/www/html/
-
-# Expor a porta 80
-EXPOSE 80
+# Ativar mod_rewrite do Apache (útil
--- php/conexao.php
+++ php/conexao.php
@@ -1,17 +1,38 @@
 <?php
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASSWORD');
-$db   = getenv('DB_NAME');
+//======Variáveis do Banco======
+$host = "db"; // Conecta-se ao servidor local ( Ou seja na máquina utilizada )
+$user = "root"; // Usuário, geralmente root
+$pass = "root"; // Senha, geralmente vazia
+$banco = "padaria_pao_genial"; // Banco de dados que será utilizado
 
+//======Criando a conexão com o banco em PDO======
 try {
-    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8";
-    $pdo = new PDO($dsn, $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$dsn = "mysql:host=$host;dbname=$banco;charset=utf8";
+$pdo = new PDO($dsn, $user, $pass);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Se o banco for conectado com sucesso, ele irá exibir uma mensagem, dizendo que o banco está executando corretamente
 
-    echo "<script>alert('Conectado com sucesso ao banco!');</script>";
-} catch (PDOException $e) {
-    die("<script>alert('Erro ao conectar-se ao banco');</script>" . $e->getMessage());
+// Configura o PDO para lançar exceções em caso de erros.
+// PDO::ATTR_ERRMODE: Define o modo de relatório de erros.
+// PDO::ERRMODE_EXCEPTION: Se ocorrer um erro na comunicação com o banco (ex: consulta SQL errada),
+// o PDO lançará uma exceção (um tipo especial de erro que pode ser "capturado" pelo bloco catch).
+// Isso é bom para depuração e para tratar erros de forma mais robusta.]
+
+
+
+
+} catch (PDOException $e ){
+    // Se ocorrer qualquer erro (uma PDOException) durante a tentativa de conexão no bloco 'try',
+    // o código dentro deste bloco 'catch' será executado.
+
+
+
+
+    // $e é um objeto que contém informações sobre o erro que ocorreu.
+    // $e->getMessage() retorna uma mensagem descrevendo o erro.
+    die("<script>alert('Erro ao conectar-se ao banco');</script>" . $e->getMessage()); // Interrompe a execução e exibe uma mensagem de erro!
+
+
+
+
 }
 ?>
